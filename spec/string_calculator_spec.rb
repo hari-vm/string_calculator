6788c636153a649e9350bd66b77799fe6462f668
Hari <harivm71@gmail.com>
2025-07-26 14:24:40 +0530

    more scenarios

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -13,5 +13,21 @@ RSpec.describe StringCalculator do
     it 'returns the sum of two comma-separated numbers' do
       expect(subject.add("1,5")).to eq(6)
     end
+
+    it 'returns the sum of multiple comma-separated numbers' do
+      expect(subject.add("1,2,3,4")).to eq(10)
+    end
+
+    it 'handles newline as a delimiter' do
+      expect(subject.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports custom delimiters' do
+      expect(subject.add("//;\n1;2")).to eq(3)
+    end
+
+    it 'raises exception for negative numbers' do
+      expect { subject.add("1,-2,3,-5") }.to raise_error("negative numbers not allowed -2,-5")
+    end
   end
 end
