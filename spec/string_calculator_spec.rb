5cac80b705ad5fc2babf47b59982ea0c24f8bd1a
Hari <harivm71@gmail.com>
2025-07-26 14:05:15 +0530

    Base functional tests

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,13 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(subject.add("")).to eq(0)
     end
+
+    it 'returns 1 for an empty string' do
+      expect(subject.add("1")).to eq(1)
+    end
+
+    it 'returns the sum of two comma-separated numbers' do
+      expect(subject.add("1,5")).to eq(6)
+    end
   end
 end
