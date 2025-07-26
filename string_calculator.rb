b06d2bd12a8ccd89355497032f2246eae0654d6e
Hari <harivm71@gmail.com>
2025-07-26 17:05:53 +0530

    Added additional logics

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,22 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    # numbers.split(',').map(&:to_i).sum
-    numbers.split(/,|\n/).map(&:to_i).sum
+    nums      = filter_numbers(numbers).map(&:to_i)
+    negatives = nums.select { |n| n < 0 }
+
+    raise "negative numbers not allowed #{negatives.join(',')}" unless negatives.empty?
+
+    nums.sum
+  end
+
+  private
+
+  def filter_numbers(numbers)
+    return numbers.split(/,|\n/) unless numbers.start_with?("//")
+        
+    delimiter, numbers = numbers.split("\n", 2)
+    delimiter          = delimiter[2]
+
+    numbers.split(/#{Regexp.escape(delimiter)}|\n/)
   end
 end
